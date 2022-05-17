46d0e13f210af752923c3d894f44934da8717759
tetsunari <mtst.tty556@gmail.com>
2022-05-17 17:59:46 +0900

    redis確認用の記述追加

--- src/app/Http/MyApp/Controller/MyAppController.php
+++ src/app/Http/MyApp/Controller/MyAppController.php
@@ -3,14 +3,25 @@
 namespace App\Http\MyApp\Controller;
 
 use App\Service\MyApp\MyAppService;
+use App\Service\Redis\RedisService;
+use Illuminate\Redis\Connections\Connection;
 
 final class MyAppController
 {
-    public function __construct(private MyAppService $MyAppService)
+    public function __construct(
+        private MyAppService $MyAppService,
+        private RedisService $RedisService,
+    )
     {}
 
     public function index()
     {
         $this->MyAppService->index();
+    }
+
+    public function redis()
+    {
+        $Redis = $this->RedisService->validConnect();
+        if ($Redis instanceof Connection) $this->RedisService->setValue($Redis);
     }
 }
