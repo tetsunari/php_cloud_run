b49d45c14fafcb729ac843fe5e750dc0f56cf494
tetsunari <mtst.tty556@gmail.com>
2022-05-17 14:39:45 +0900

    service, interface create

--- src/app/Service/MyApp/MyAppInterface.php
+++ src/app/Service/MyApp/MyAppInterface.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\Service\Interface;
+namespace App\Service\MyApp;
 
 interface MyAppInterface
 {
--- src/app/Service/MyApp/MyAppService.php
+++ src/app/Service/MyApp/MyAppService.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
+namespace App\Service\MyApp;
+
 final class MyAppService
 {
     public function __construct(private MyAppInterface $MyAppInterface)
