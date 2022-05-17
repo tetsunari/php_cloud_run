a38f9bf71d5ceb2ff577bf3f65030791662ab67d
tetsunari <mtst.tty556@gmail.com>
2022-05-17 17:58:48 +0900

    redis用のservice作成

--- src/app/Service/Redis/RedisInterface.php
+++ src/app/Service/Redis/RedisInterface.php
@@ -1,8 +1,11 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Service\Redis;
 
+use Illuminate\Redis\Connections\Connection;
+
 interface RedisInterface
 {
-
+    public function validConnect(): ?Connection;
+    public function setValue(Connection $Redis);
 }
--- src/app/Service/Redis/RedisService.php
+++ src/app/Service/Redis/RedisService.php
@@ -1,8 +1,21 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Service\Redis;
 
-class RedisService
-{
+use Illuminate\Redis\Connections\Connection;
 
+final class RedisService
+{
+    public function __construct(private RedisInterface $RedisInterface)
+    {}
+
+    public function validConnect(): ?Connection
+    {
+        return $this->RedisInterface->validConnect();
+    }
+
+    public function setValue(Connection $Redis)
+    {
+        $this->RedisInterface->setValue($Redis);
+    }
 }
